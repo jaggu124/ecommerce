86e65f184545d5b61aaff8bfbd7e1a3c052bf0a8
jaggu124 <dhanavathjagadish2002@gmail.com>
2026-08-14 19:18:17 +0530

    config changes

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
  
-export const API_BASE_URL = "http://localhost:5454"
+export const API_BASE_URL = "https://ecommerce-server-production-acb9.up.railway.app"
  
 export const api = axios.create({
     baseURL: API_BASE_URL,
